aa9f126118ea90ae5afd4a287ef2977a89935ec8
Матвей <roganov94@mail.ru>
2023-05-22 16:41:06 +0300

    Проработал класс и сделал тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def product():
+    return Item("Утюг", 5000, 25)
+
+
+def test_calculate_total_price(product):
+    assert product.calculate_total_price() == 125000
+
+
+def test_apply_discount(product):
+    assert product.apply_discount() is None
